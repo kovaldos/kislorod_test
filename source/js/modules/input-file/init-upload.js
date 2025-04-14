3cdefc2b4454f7a34e6261702f976f0d27c656f7
kovaldos <ya.kovalev@ya-kovalev.ru>
2025-04-15 00:39:42 +0300

    fix: bugfix

--- source/js/modules/input-file/init-upload.js
+++ source/js/modules/input-file/init-upload.js
@@ -2,23 +2,24 @@ import {Upload} from './upload.js';
 
 const uploadImageBlock = document.querySelector('[data-upload="img"]');
 
+const ONE_MB = 1048576;
+
 export const uploadImage = () => new Upload(uploadImageBlock, {
-  uploadLength: 4,
+  uploadLength: 99,
   dropZone: false,
   preview: true,
   previewImg: true,
-  maxFileSize: 10485760,
-  accept: ['.png', '.jpg', '.jpeg'],
+  maxFullSize: ONE_MB * 10,
+  accept: ['.png', '.jpeg'],
   iconFormat: {
     png: './img/file/icon-file-png.png',
-    jpg: './img/file/icon-file-jpg.png',
     jpeg: './img/file/icon-file-jpg.png',
     default: './img/file/icon-file-docs.png',
   },
   fileInfo: {
-    fileName: true,
-    fileSize: true,
+    fileName: false,
+    fileSize: false,
   },
-  emptyMessage: 'Добавьте до 4 изображений (Необязательно)',
-  errorMessage: 'Выделенные файлы превышают максимальный размер',
+  emptyMessage: '',
+  errorMessage: 'Размер фотографии(ий) превышает 10 Мб!',
 });
